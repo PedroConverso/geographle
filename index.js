3359f7d1620cab6c43e256e7fe3abb80d3f4f019
PedroConverso <conversopedro@gmail.com>
2024-09-03 15:17:13 -0300

    commit connections 1

--- index.js
+++ index.js
@@ -1,5 +1,31 @@
-import readlineSync from "readline-sync";
-import fs, { writeFileSync } from "fs"; // Importar el módulo fs
+import fs from 'fs';
 
-let data = JSON.parse(fs.readFileSync("data/Connections.json", 'utf8'));
-console.log(data);
+const rutaArchivo = 'data/Connections.json';
+
+function obtenerPaisesYPalabrasAleatorias() {
+    try {
+        const datos = JSON.parse(fs.readFileSync(rutaArchivo, 'utf8'));
+
+        if (datos.length === 0) {
+            return 'No hay datos disponibles.';
+        }
+
+        const datosAleatorios = datos.sort(() => 0.5 - Math.random());
+
+        const paisesSeleccionados = datosAleatorios.slice(0, 4);
+
+        const resultado = paisesSeleccionados.map(function(paisDatos) {
+            return {
+                pais: paisDatos.nombre_pais,
+                palabras: paisDatos.words_related.slice(0, 4)
+            };
+        });
+
+        return resultado;
+    } catch (error) {
+        return `Error al leer o analizar el archivo: ${error.message}`;
+    }
+}
+
+const paisesYPalabrasAleatorias = obtenerPaisesYPalabrasAleatorias();
+console.log(paisesYPalabrasAleatorias);
