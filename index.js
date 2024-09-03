ffcc2a43cb72537ec2410a16709eaf0a07621ffc
PedroConverso <conversopedro@gmail.com>
2024-09-03 11:30:39 -0300

    Update index.js

--- index.js
+++ index.js
@@ -3,4 +3,3 @@ import fs, { writeFileSync } from "fs"; // Importar el módulo fs
 
 let data = JSON.parse(fs.readFileSync("data/Connections.json", 'utf8'));
 console.log(data);
-JSON.stringify(fs.writeFileSync("data/Connections.json", 'utf8')) 
