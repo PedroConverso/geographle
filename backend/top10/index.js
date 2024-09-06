f8f70f4f75199d44f723d79dab27f01515a30e17
pepegon08 <pedrogonzalez180808@gmail.com>
2024-09-06 10:38:02 -0300

    Update index.js

--- backend/top10/index.js
+++ backend/top10/index.js
@@ -1,6 +1,7 @@
 import fs from 'fs'
+import readline from 'node:readline'
 
-const data = fs.readFileSync('data/Top_10.json');
+const data = fs.readFileSync('C:/github/proyecto-3-geographle/data/Top_10.json' , 'utf-8');
 const { paises } = JSON.parse(data);
 
 console.log(data);
