985efeb03684e2da3bc33d9469e4c0a52c5f7a98
pepegon08 <pedrogonzalez180808@gmail.com>
2024-09-06 09:37:34 -0300

    order

--- backend/conections/index.js
+++
@@ -1,5 +0,0 @@
-import readlineSync from "readline-sync";
-import fs, { writeFileSync } from "fs"; // Importar el módulo fs
-
-let data = JSON.parse(fs.readFileSync("data/Connections.json", 'utf8'));
-console.log(data);
