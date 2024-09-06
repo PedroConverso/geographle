0311443633a2b251f34a7865873655bc5d1cfcc8
VITACCA AZRAK FEDERICO JOSE <49004081@ort>
2024-09-06 15:28:00 -0300

    fin connections

---
+++ Connections/Connections,1 (normal)/Connections,2 (normal)/Connections,2.js
@@ -0,0 +1,53 @@
+function openmenudropdown() {
+    let menu = document.getElementById("menudropdown")
+    if (menu.classList.contains("open")) {
+        menu.classList.remove("open")
+    } else {
+        menu.classList.add("open")
+    }
+}
+
+function openestadown() {
+    let menu = document.getElementById("estadown")
+    if (menu.classList.contains("edOpen")) {
+        menu.classList.remove("edOpen")
+    } else {
+        menu.classList.add("edOpen")
+    }
+}
+
+function optisdown() {
+    let menu = document.getElementById("optidown")
+    if (menu.classList.contains("edSet")) {
+        menu.classList.remove("edSet")
+    } else {
+        menu.classList.add("edSet")
+    }
+}
+
+function thememode() {
+    let menu = document.getElementById("themeMode-check-container")
+    if (menu.classList.contains("themeMode-check-container-on")) {
+        menu.classList.remove("themeMode-check-container-on")
+    } else {
+        menu.classList.add("themeMode-check-container-on")
+    }
+}
+
+function thememode2() {
+    let menu = document.getElementById("themeMode-check-container2")
+    if (menu.classList.contains("themeMode-check-container-on2")) {
+        menu.classList.remove("themeMode-check-container-on2")
+    } else {
+        menu.classList.add("themeMode-check-container-on2")
+    }
+}
+
+function infodown() {
+    let menu = document.getElementById("infodown")
+    if (menu.classList.contains("edinf")) {
+        menu.classList.remove("edinf")
+    } else {
+        menu.classList.add("edinf")
+    }
+}
