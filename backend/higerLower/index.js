a1adcfc36d499e0d18469c6f3e163c8b72d214bb
pepegon08 <pedrogonzalez180808@gmail.com>
2024-09-20 10:28:24 -0300

    nsdvddcc

--- backend/higerLower/index.js
+++ backend/higerLower/index.js
@@ -1,1 +1,7 @@
 import fs from 'fs';
+import readline from 'readline';
+import { onEvent, startServer } from "soquetic"
+
+
+const data = JSON.parse(fs.readFileSync('higher_or_lower.json', 'utf8'));
+console.log(data);
