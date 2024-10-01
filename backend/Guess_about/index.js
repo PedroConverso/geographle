ce6a541d6d7c88427ee50bb3f434b42fd1c031eb
PedroConverso <conversopedro@gmail.com>
2024-10-01 13:25:44 -0300

    server con connections y guess about

--- backend/Guess_about/index.js
+++ backend/Guess_about/index.js
@@ -1,16 +1,15 @@
 import fs from 'fs';
-import { onEvent, sendEvent, startServer } from "soquetic";
 
 let datos = JSON.parse(fs.readFileSync('./data/Guess_about.json', 'utf8'));
-let vidasBandera = 3; // Vidas para la ronda de la bandera
-let vidasGenerales = 5; // Vidas totales para las otras rondas
+let vidasBandera = 3; 
+let vidasGenerales = 5;
 let paisActual = null;
 
-function obtenerPaisAleatorio() {
+export function obtenerPaisAleatorio() {
   return datos[Math.floor(Math.random() * datos.length)];
 }
 
-function generarOpciones(prop) {
+export function generarOpciones(prop) {
   let opciones = new Set();
   for (let i = 0; i < datos.length; i++) {
     opciones.add(datos[i][prop]);
@@ -22,61 +21,61 @@ function generarOpciones(prop) {
   return opciones.sort(() => 0.5 - Math.random());
 }
 
-function verificarRespuesta(prop, respuesta) {
+export function verificarRespuesta(prop, respuesta) {
   return paisActual[prop].toLowerCase() === respuesta.toLowerCase();
 }
 
-// Función para obtener la bandera sin opciones de respuesta
-function obtenerFlag() {
+
+export function obtenerFlag() {
   paisActual = obtenerPaisAleatorio();
   return {
     flag: paisActual.flag
   };
 }
 
-function obtenerOpcionesIdioma() {
+export function obtenerOpcionesIdioma() {
   return {
     language_options: generarOpciones('language')
   };
 }
 
-function obtenerOpcionesCapital() {
+export function obtenerOpcionesCapital() {
   return {
     capital_options: generarOpciones('capital')
   };
 }
 
-function obtenerOpcionesForma() {
+export function obtenerOpcionesForma() {
   return {
     shape_options: generarOpciones('country_shape')
   };
 }
 
 // Verificación de respuestas
-function verificarRespuestaFlag(respuesta) {
-  return verificarRespuestaGeneral('name', respuesta, 'bandera'); // Ronda de bandera
+export function verificarRespuestaFlag(respuesta) {
+  return verificarRespuestaGeneral('name', respuesta, 'bandera');
 }
 
-function verificarRespuestaIdioma(respuesta) {
-  return verificarRespuestaGeneral('language', respuesta, 'general'); // Rondas generales
+export function verificarRespuestaIdioma(respuesta) {
+  return verificarRespuestaGeneral('language', respuesta, 'general');
 }
 
-function verificarRespuestaCapital(respuesta) {
-  return verificarRespuestaGeneral('capital', respuesta, 'general'); // Rondas generales
+export function verificarRespuestaCapital(respuesta) {
+  return verificarRespuestaGeneral('capital', respuesta, 'general');
 }
 
-function verificarRespuestaForma(respuesta) {
-  return verificarRespuestaGeneral('country_shape', respuesta, 'general'); // Rondas generales
+export function verificarRespuestaForma(respuesta) {
+  return verificarRespuestaGeneral('country_shape', respuesta, 'general'); 
 }
 
-function verificarRespuestaGeneral(prop, respuesta, tipoRonda) {
+export function verificarRespuestaGeneral(prop, respuesta, tipoRonda) {
   const esCorrecta = verificarRespuesta(prop, respuesta);
 
   if (!esCorrecta) {
     if (tipoRonda === 'bandera') {
-      vidasBandera--; // Restar vida si la respuesta de la bandera es incorrecta
+      vidasBandera--; 
     } else {
-      vidasGenerales--; // Restar vida si la respuesta de las rondas generales es incorrecta
+      vidasGenerales--; 
     }
   }
 
@@ -103,15 +102,4 @@ function verificarRespuestaGeneral(prop, respuesta, tipoRonda) {
   };
 }
 
-// Eventos
-onEvent("obtenerFlag", obtenerFlag);
-onEvent("obtenerOpcionesIdioma", obtenerOpcionesIdioma);
-onEvent("obtenerOpcionesCapital", obtenerOpcionesCapital);
-onEvent("obtenerOpcionesForma", obtenerOpcionesForma);
-onEvent("verificarRespuestaFlag", verificarRespuestaFlag);
-onEvent("verificarRespuestaIdioma", verificarRespuestaIdioma);
-onEvent("verificarRespuestaCapital", verificarRespuestaCapital);
-onEvent("verificarRespuestaForma", verificarRespuestaForma);
-
-// Iniciar servidor
 startServer(3000);
